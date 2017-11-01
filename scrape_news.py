29b74f66d0ee4d924488a1c2286d9a18156a4c9b
jackoe <jackwines@mac.com>
2017-11-01 18:02:11 -0500

    scraping works now

--- scrape_news.py
+++ scrape_news.py
@@ -1,53 +1,63 @@
 import urllib.request
 from bs4 import BeautifulSoup
+from functools import reduce
 import bs4
 # pip3 install beautifulsoup4
 import sys
 
-def grab_soup_kanji(soup):
+def tuple_string_add(t1 ,t2):
+    (y1, y2) = ("", "")
+    if len(t2) == 2:
+        (x2, y2) = t2
+    elif len(t2) == 3:
+        (x2, y21, y22) = t2
+        y1 = y21 + y22
+    else:
+        (x21, y21, x22, y22) = t2
+        x2 = x21 + x22
+        y2 = y21 + y22
+    (x1, y1) = t1
+    return (x1+x2, y1+y2)
+
+def tuple_list_flatten(l):
+    return reduce(tuple_string_add, l, ("", ""))
+
+def grab_soup_kanji_katakana(soup):
     # if the paragraph tag is a string, it's just an empty string
     if type(soup) == str:
-        return soup
-    #print(soup, type(soup))
+        return soup, soup
     elif type(soup) == bs4.element.NavigableString:
-        return str(soup)
+        katakana = str(soup)
+        return katakana, katakana
     else:
-        print(soup.span)
-        print([x for x in soup])
-        #print(soup.find_all("ruby"))
-    #if soup.unicode[:6] == "ruby":
-    #    print(soup)
-    #elif type(soup) is bs4.element.Tag
-    #   print(type(soup), soup.ruby)
-    #print(type(soup), 'DONE')
-    #print(soup.find_all('ruby'))
-#    print(soup, "TYPE", type(soup), "\n")
-#    print([type(x) for x in soup])
-    return ""
+        if len(list(soup.strings)) == 1:
+            soup_str = list(soup.strings)[0]
+            return (soup_str, soup_str)
+        return tuple(soup.strings)
 
-def grab_soup_hiragana(soup):
-    return ""
-    
-
-# takes paragraph as soup tag
-# outputs list (of sentences) of tuples
+# outputs tuple of strings
 # (kanji_str, hirigana_str)
 def parse_paragraph(paragraph_soup):
-    #print(type(paragraph_soup))
-    words = [(grab_soup_kanji(word), grab_soup_hiragana(word)) 
-                for word in paragraph_soup]
-    
+    words = [grab_soup_kanji_katakana(word) for word in paragraph_soup]
+    return (tuple_list_flatten(words))
+
 
 def grab_sentences(input_html):
     soup = BeautifulSoup(input_html, 'html.parser')
     soup = soup.find(id="newsarticle")
+    for x in soup.find_all("span") + soup.find_all('a'):
+        x.unwrap()
     parsed_graphs = [parse_paragraph(x) for x in soup]
+    return tuple_list_flatten(parsed_graphs)
 
 
-#urls = sys.stdin.read().splitlines()
-urls = ["http://www3.nhk.or.jp/news/easy/k10011198291000/k10011198291000.html"]
+urls = sys.stdin.read().splitlines()
+#urls = ["http://www3.nhk.or.jp/news/easy/k10011198291000/k10011198291000.html"]
 # GET request howto from the following
 # https://stackoverflow.com/questions/645312/what-is-the-quickest-way-to-http-get-in-python
-for url in urls:
-    grab_sentences(urllib.request.urlopen(url).read())
+parsed_pages = [grab_sentences(urllib.request.urlopen(url).read()) for url in urls]
+(all_kanji, all_katakana) = (tuple_list_flatten(parsed_pages))
+all_kanji    =    all_kanji.replace('\n', '').split('。')
+all_katakana = all_katakana.replace('\n', '').split('。')
 
+print(all_kanji, all_katakana)
